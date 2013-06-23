e5e495705dfffea53086c0e1c157844f3c85d6ca
Seetha <nseetha2006@gmail.com>
2013-06-23 17:18:50 +0530

    Signed-off-by: Seetha <nseetha2006@gmail.com>

---
+++ src/com/mycompany/dao/hibernate/RoleDaoHibernate.java
@@ -0,0 +1,47 @@
+package com.mycompany.dao.hibernate;
+
+import java.util.List;
+
+import com.mycompany.dao.RoleDao;
+import com.mycompany.model.Role;
+import org.springframework.stereotype.Repository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.hibernate.SessionFactory;
+
+
+/**
+ * This class interacts with Spring's HibernateTemplate to save/delete and
+ * retrieve Role objects.
+ *
+ * @author <a href="mailto:bwnoll@gmail.com">Bryan Noll</a> 
+ */
+@Repository
+public class RoleDaoHibernate extends GenericDaoHibernate<Role, Long> implements RoleDao {
+
+    /**
+     * Constructor to create a Generics-based version using Role as the entity
+     */
+    public RoleDaoHibernate() {
+        super(Role.class);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public Role getRoleByName(String rolename) {
+        List roles = getHibernateTemplate().find("from Role where name=?", rolename);
+        if (roles.isEmpty()) {
+            return null;
+        } else {
+            return (Role) roles.get(0);
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void removeRole(String rolename) {
+        Object role = getRoleByName(rolename);
+        getHibernateTemplate().delete(role);
+    }
+}
